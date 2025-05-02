efa7522cda4cecb5ba4be44c170bb4e7de5dcf9a
samim3ini <sjm111cia@gmail.com>
2025-05-02 04:28:08 +0300

    1.0.7

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',            // ensures all asset URLs start with "/"
-  plugins: [react()],
-  build: {
-    outDir: 'dist',      // where Vite writes the production build
-    assetsDir: 'assets', // subfolder for JS/CSS/images
-    sourcemap: false     // optional, omit if you don’t need source maps
-  }
+  plugins: [react()]
 })
